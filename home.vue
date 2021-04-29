84b8d8fa4390606d4630f7765a073dd028fd50d3
Nikita Nirhali <62387781+nikitanirhali@users.noreply.github.com>
2021-04-29 13:59:00 +0530

    Version 2

--- home.vue
+++ home.vue
@@ -1,46 +1,37 @@
 <template>
  <div  >
-    <b-navbar toggleable="sm" type="light" variant="light"  class="navbar">
-      
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  
 
-        <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item >
-            <b-icon icon="list" variant="dark"></b-icon>Saved List</b-nav-item>
-          <b-nav-item>
-            <b-icon icon="funnel" variant="dark"></b-icon>Saved Filters
-            </b-nav-item>
-          <b-nav-item >
-            <b-icon icon="clock-history" variant="dark" v-model="query" ></b-icon>Serach History</b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <!-- <b-form-input size="sm" class="mr-sm-2" placeholder="Search" v-model="search"></b-form-input> -->
-            
-            <b-form-input
-             type="text"
-             name=""
-             
-              placeholder="Search...."
-              
-               v-model="filter"
-            ></b-form-input>
-            <!-- id="myinput" -->
-       <!-- v-on:keyup="searchfun()" -->
-          </b-nav-form>
-          
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" ><b-icon icon="list" variant="dark"></b-icon>Saved List<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" ><b-icon icon="funnel" variant="dark"></b-icon>Saved Filters</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link ">
+          <b-icon icon="clock-history" variant="dark" v-model="query" ></b-icon>Serach History
+        </a>
+        
+      </li>
+     
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search"  id="myInput" v-on:keyup="myFunction()"  title="Type in a name">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
 <div class="content">
     <div class="col">
-      <div>
-        <div>
-      <table class="table ">
+      
+    <div>
+      <table class="table " id="myTable">
         <thead>
-        <tr >
+        <tr  class="header">
           <th scope="col" >Date</th>
           <th scope="col">List Name</th >
          <th scope="col">  No Of Entities</th >
@@ -61,20 +52,21 @@
             
            </td>
            <td>
-           <b-button class="my-2 my-sm-0" @click="showdetails(item.id)" >Details</b-button> 
+           <b-button class="my-2 my-sm-0" @click="showdetails(item.details)" >Details</b-button> 
+           
             </td> 
             
           </tr>
          </tbody>
       </table>
       </div>
-      </div>
+      
     </div>
     <div class="col">
       <h4 class="heading"> Details</h4>
       <table class="table">
-        <tr v-for="detail in dtl" :key="detail.id">
-          <td>{{ detail}}</td>
+        <tr v-for="a in arr" :key="a.id">
+          <td>{{a}}</td>
         </tr>
       </table>
 
@@ -86,66 +78,67 @@
 
 <script>
 // import data from './plugins/data';
-import RawData from "D:/vue practice/tableassign/src/RawData.json"
+// import RawData from "D:/vue practice/tableassign/src/RawData.json"
 export default{
   
 
   name : "home",
-  dtl:RawData,
+  
   data() {
     return {
       //   // Note `isActive` is left out and will not appear in the rendered table
-      
-      query:'',
-      originalrows:[], 
+      // dtl:RawData,
+      search:'',
+      filter:"", table:"", tr:"", td:"", i:"", txtValue:"",
+      arr:[],
        fields: ['Date', 'ListName', 'NoOfEntities','Actions'],
         items: [
-           {  Date: '02/02/2020', ListName: 'Book', NoOfEntities: '2' },
-           {  Date: '01/02/2020', ListName: 'Pen', NoOfEntities: '2' },
-            {  Date:'03/02/2020', ListName: 'Eraser', NoOfEntities: '2' },
-            { Date: '01/04/2020', ListName: 'Ink', NoOfEntities: '2' }
+           {  Date: '02/02/2020', ListName: 'Book', NoOfEntities: '2',details:["Name:Nikita ","Age:23"] },
+           {  Date: '01/02/2020', ListName: 'Pen', NoOfEntities: '2',details:["Name:Nikhil ","Age:23"] },
+            {  Date:'03/02/2020', ListName: 'Eraser', NoOfEntities: '2',details:["Name:Mousam ","Age:23"] },
+            { Date: '01/04/2020', ListName: 'Ink', NoOfEntities: '2',details:["Name:Prathamesh ","Age:23"] }
          ]
         // data:data
     }
 
    
   },
-  beforeMount(){
-    this.originalrows=this.items;
-  },
-  methods:{
-    search(){
-      var result=[];
-      var searchData=this.originalrows;
-      if(this.query==""){
-        this.items=this.originalrows;
-      }
-      else{
-        for(var i=0;i< searchdata.length;i++){
-          var sparam =this.query.toLowerCase();
-          for(var key in searchData[i]){
-            if(searchData[i].hasOwnProperty(key)){
-              var value=searchData[i][key];
-              if(typeof value =="string" && value.toLowerCase().indexOf(sparam) >= 0){
-                result.push(searchData[i]);
-              }
-            }
-          }
-        }
-        this.items=result;
-      }
-    },
-   showdetails(){
-      var id=item.id;
-      return dtl.detail[id];
-    
-
-
-   }
-  }
+methods:{
+ myFunction() {
+  // Declare variables
  
+  this.input = document.getElementById("myInput").value;
+  this.filter = document.getElementById("myInput").value.toUpperCase();
+  this.table = document.getElementById("myTable");
+  this.tr = table.getElementsByTagName("tr");
 
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < this.tr.length; i++) {
+    this.td = this.tr[i].getElementsByTagName("td")[1];
+    if (this.td) {
+      this.txtValue = this.td.textContent || this.td.innerText;
+      if (this.txtValue.toUpperCase().indexOf(this.filter) == -1) {
+        this.tr[i].style.display = "none";
+        
+      } else {
+        this.tr[i].style.display = "";
+       
+      }
+    }
   }
+
+},
+        
+showdetails(arr){
+  this.arr=arr;
+
+}
+    
+}
+  
+ 
+}
+  
    
 
 </script>
